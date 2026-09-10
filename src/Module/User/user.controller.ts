14bd34bee3282d4a57c66e6e4bb5fd6e24c7b378
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-09-10 06:05:18 +0000

    fix

--- src/Module/User/user.controller.ts
+++ src/Module/User/user.controller.ts
@@ -30,101 +30,101 @@ class UserController extends BaseController {
   });
 
   // login the user
-  loginUser = asyncHandler(async (req: Request, res: Response) => {
-    const { loginInfo, password } = req.body as {
-      loginInfo: string;
-      password: string;
-    };
-    if (!loginInfo?.trim() || !password?.trim())
-      throw new ApiError(400, "All fields are required");
+  // loginUser = asyncHandler(async (req: Request, res: Response) => {
+  //   const { loginInfo, password } = req.body as {
+  //     loginInfo: string;
+  //     password: string;
+  //   };
+  //   if (!loginInfo?.trim() || !password?.trim())
+  //     throw new ApiError(400, "All fields are required");
 
-    const { loginData, accessToken, refreshToken } =
-      await userService.loginUser(loginInfo, password);
+  //   const { loginData, accessToken, refreshToken } =
+  //     await userService.loginUser(loginInfo, password);
 
-    return res
-      .status(200)
-      .clearCookie("OtpToken")
-      .cookie("AccessToken", accessToken, this.cookieOptions)
-      .cookie("RefreshToken", refreshToken, this.cookieOptions)
-      .json(
-        new ApiResponse(200, "Logged in successfully", {
-          user: loginData,
-        }),
-      );
-  });
-
-
-  //me
-
-  me = asyncHandler(async(req:Request, res:Response)=>{
-    const user = await userService.me(this.getUserId(req));
-    return res
-      .status(200)
-      .json(new ApiResponse(200, "Logged out successfully" ,user));
-  })
+  //   return res
+  //     .status(200)
+  //     .clearCookie("OtpToken")
+  //     .cookie("AccessToken", accessToken, this.cookieOptions)
+  //     .cookie("RefreshToken", refreshToken, this.cookieOptions)
+  //     .json(
+  //       new ApiResponse(200, "Logged in successfully", {
+  //         user: loginData,
+  //       }),
+  //     );
+  // });
 
 
-  //logout user
-  logoutUser = asyncHandler(async (req: Request, res: Response) => {
-    await userService.logoutUser(this.getUserId(req));
-    return res
-      .status(200)
-      .clearCookie("AccessToken", this.cookieOptions)
-      .clearCookie("RefreshToken", this.cookieOptions)
-      .json(new ApiResponse(200, "Logged out successfully"));
-  });
+  // //me
 
-  // reset refresh token
-  resetRefreshToken = asyncHandler(async (req: Request, res: Response) => {
-    const incomingRefToken =
-      req.cookies?.RefreshToken ||
-      (req.headers["refreshtoken"] as string | undefined);
-    if (!incomingRefToken) throw new ApiError(401, "Unauthorized access");
+  // me = asyncHandler(async(req:Request, res:Response)=>{
+  //   const user = await userService.me(this.getUserId(req));
+  //   return res
+  //     .status(200)
+  //     .json(new ApiResponse(200, "Logged out successfully" ,user));
+  // })
 
-    const { accessToken, refreshToken } =
-      await userService.resetRefreshToken(incomingRefToken);
 
-    return res
-      .status(200)
-      .cookie("AccessToken", accessToken, this.cookieOptions)
-      .cookie("RefreshToken", refreshToken, this.cookieOptions)
-      .json(new ApiResponse(200, "Token updated successfully"));
-  });
+  // //logout user
+  // logoutUser = asyncHandler(async (req: Request, res: Response) => {
+  //   await userService.logoutUser(this.getUserId(req));
+  //   return res
+  //     .status(200)
+  //     .clearCookie("AccessToken", this.cookieOptions)
+  //     .clearCookie("RefreshToken", this.cookieOptions)
+  //     .json(new ApiResponse(200, "Logged out successfully"));
+  // });
 
-  //forgot password
-  forgotPassword = asyncHandler(async (req: Request, res: Response) => {
-    const { email } = req.body as { email: string };
-    if (!email?.trim()) throw new ApiError(400, "Give the fields");
+  // // reset refresh token
+  // resetRefreshToken = asyncHandler(async (req: Request, res: Response) => {
+  //   const incomingRefToken =
+  //     req.cookies?.RefreshToken ||
+  //     (req.headers["refreshtoken"] as string | undefined);
+  //   if (!incomingRefToken) throw new ApiError(401, "Unauthorized access");
 
-    const otp = await userService.initForgotPassword(email);
-    return res.status(200).json(new ApiResponse(200, "OTP generated", { otp }));
-  });
+  //   const { accessToken, refreshToken } =
+  //     await userService.resetRefreshToken(incomingRefToken);
 
-  //verify otp for password
-  verifyPasswordChangeOtp = asyncHandler(
-    async (req: Request, res: Response) => {
-      const { email, otp } = req.body as { email: string; otp: string };
-      if (!email || !otp) throw new ApiError(400, "Give the fields");
+  //   return res
+  //     .status(200)
+  //     .cookie("AccessToken", accessToken, this.cookieOptions)
+  //     .cookie("RefreshToken", refreshToken, this.cookieOptions)
+  //     .json(new ApiResponse(200, "Token updated successfully"));
+  // });
 
-      const otpToken = await userService.verifyOtp(email, otp);
-      return res
-        .status(200)
-        .cookie("OtpToken", otpToken, this.cookieOptions)
-        .json(new ApiResponse(200, "OTP verified"));
-    },
-  );
+  // //forgot password
+  // forgotPassword = asyncHandler(async (req: Request, res: Response) => {
+  //   const { email } = req.body as { email: string };
+  //   if (!email?.trim()) throw new ApiError(400, "Give the fields");
 
-  // update password
-  updatePassword = asyncHandler(async (req: Request, res: Response) => {
-    const { password } = req.body as { password: string };
-    if (!password?.trim()) throw new ApiError(400, "Give a password");
+  //   const otp = await userService.initForgotPassword(email);
+  //   return res.status(200).json(new ApiResponse(200, "OTP generated", { otp }));
+  // });
 
-    await userService.updatePassword(this.getUserId(req), password);
-    return res
-      .status(200)
-      .clearCookie("OtpToken", this.cookieOptions)
-      .json(new ApiResponse(200, "Password changed"));
-  });
+  // //verify otp for password
+  // verifyPasswordChangeOtp = asyncHandler(
+  //   async (req: Request, res: Response) => {
+  //     const { email, otp } = req.body as { email: string; otp: string };
+  //     if (!email || !otp) throw new ApiError(400, "Give the fields");
+
+  //     const otpToken = await userService.verifyOtp(email, otp);
+  //     return res
+  //       .status(200)
+  //       .cookie("OtpToken", otpToken, this.cookieOptions)
+  //       .json(new ApiResponse(200, "OTP verified"));
+  //   },
+  // );
+
+  // // update password
+  // updatePassword = asyncHandler(async (req: Request, res: Response) => {
+  //   const { password } = req.body as { password: string };
+  //   if (!password?.trim()) throw new ApiError(400, "Give a password");
+
+  //   await userService.updatePassword(this.getUserId(req), password);
+  //   return res
+  //     .status(200)
+  //     .clearCookie("OtpToken", this.cookieOptions)
+  //     .json(new ApiResponse(200, "Password changed"));
+  // });
 
 }
 
